81b76dc477ba8f61e508bdd3196cf2bead27cac0
Alexander Twigg <Alexander.S.Twigg-1@ou.edu>
2019-04-29 15:26:28 -0500

    commit

--- SpotifyAPIAPP/SpotifyAPIAPP/MainWindow.xaml.cs
+++ SpotifyAPIAPP/SpotifyAPIAPP/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Alexander Twigg
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
 
 namespace SpotifyAPIAPP
 {
